7b255de4ead2cc6573e5f1d4b8217612efdfee95
Ky Ky <kyky@kyky.ky>
2024-11-16 21:15:12 +0300

    *

--- trees/bst-persistent/bst.js
+++ trees/bst-persistent/bst.js
@@ -9,6 +9,60 @@ export function insertRecur(tree, val) {
     return [x, left, insertRecur(right, val)];
 }
 
+export function insertIterative(tree, val) {
+  if (tree === null)
+    return [val, null, null];
+
+  let curr,   // current node of old tree
+      dir,    // is current node left or right son
+      tree__; // new root
+  let [x, left, right] = tree;
+
+  if (val <= x) {
+    // reuse right subree and go left
+    tree__ = [x, null, right];
+    curr = left;
+    dir = 'left'; // current node is left son
+  } else {
+    // reuse left subree and go right
+    tree__ = [x, left, null];
+    curr = right;
+    dir = 'right'; // current node is right son
+  }
+
+  let prev__,  // prev node of new tree
+               // current node is connected to it
+      curr__;  // current node of new tree
+
+  prev__ = tree__;
+
+  while (true) {
+    if (curr === null) {
+      prev__[dir === 'left' ? 1 : 2] = [val, null, null];
+      break;
+    }
+
+    [x, left, right] = curr;
+
+    if (val <= x) {
+      curr__ = [x, null, right];
+      prev__[dir === 'left' ? 1 : 2] = curr__;
+
+      prev__ = curr__;
+      curr = left;
+      dir = 'left';
+    } else {
+      curr__ = [x, left, null];
+      prev__[dir === 'left' ? 1 : 2] = curr__;
+
+      prev__ = curr__;
+      curr = right;
+      dir = 'right';
+    }
+  }
+  return tree__;
+}
+
 export function toDigraph(tree) {
   function recur (tree) {
     let [val, left, right] = tree;    
